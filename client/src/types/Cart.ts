a8999f47422fb1536f2459563596481c6dda04a8
Michael Testut <michael.testut@yahoo.com>
2023-11-26 20:42:36 -0800

    start add to cart

---
+++ client/src/types/Cart.ts
@@ -0,0 +1,29 @@
+export type CartItem = {
+    image: string | undefined;
+    slug: string;
+    quantity: number;
+    countInStock: number;
+    price: number;
+    size: string;
+    _id: string;
+    name: string;
+}
+
+export type ShippingAddress = {
+    fullName:string;
+    address: string;
+    city: string;
+    state: string;
+    country: string;
+    postalCode: string;
+}
+
+export type Cart = {
+    cartItems: CartItem[];
+    shippingAddress: ShippingAddress;
+    paymentMethod: string;
+    itemsPrice: number;
+    shippingPrice: number;
+    taxPrice: number;
+    totalPrice: number;
+}
